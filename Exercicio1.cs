a922c4f130ad1cc078893514e69d901b9c2f1b41
unknown <raphasec@gmail.com>
2022-04-10 21:34:30 -0300

    primeiro commit

---
+++ Exercicio1.cs
@@ -0,0 +1,19 @@
+public class Exercicio1
+{
+    public void Questao(){
+        int x, y;
+
+        Console.WriteLine("Digite o primeiro número: ");
+        x = Convert.ToInt32(Console.ReadLine());
+         Console.WriteLine("Digite o segundo número: ");
+        y = Convert.ToInt32(Console.ReadLine());
+
+        if (x > y){
+            Console.WriteLine($"O número {x} é o maior.");
+        }else if(y > x){
+            Console.WriteLine($"O número {y} é o maior.");
+        }else{
+            Console.WriteLine("Os números são iguais.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿class Program
+{
+    static void Main(string[] args){
+        int repet = 0;
+        do{
+            Console.Clear();
+            Console.WriteLine("Escolha qual exercício deseja abrir (1-24): ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            switch(x){
+                case 1:
+                    string resp;
+                    Console.Clear();
+                    repet = 0;
+                    Exercicio1 ex1 = new Exercicio1();
+                    ex1.Questao();
+                    Console.WriteLine("Deseja tentar novamente? [S/N]");
+                    resp = Console.ReadLine();
+                    resp = resp.ToUpper();
+                    if(resp == "S"){
+                        repet = 1;
+                        Console.Clear();
+                    }else{
+                        break;
+                    }
+                    break;
+                case 2:
+                    break;
+                default:
+                    repet = 1;
+                    Console.WriteLine("Erro!!! Tente novamente pressionando ENTER");
+                    Console.ReadKey();
+                    Console.Clear();
+                    break;
+            }
+        }while(repet != 0);
+    }
+}
